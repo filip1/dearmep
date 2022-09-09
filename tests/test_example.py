a3b7220a90fb9fa20db5ba2e0e303fe18c0ef05b
Tim Weber <scy@scy.name>
2022-09-09 08:39:54 +0000

    Remove initial concept code
    I can always refer to it later again, but right now it’s in the way.

--- tests/test_example.py
+++
@@ -1,7 +0,0 @@
-from fastapi.testclient import TestClient
-
-
-def test_constraints(client: TestClient):
-    res = client.get("/constraints")
-    assert res.status_code == 200
-    assert "schedules" in res.json()
