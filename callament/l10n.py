9aec6d7c0198adaf598f318667bed1daa6464a9c
Tim Weber <scy@scy.name>
2022-09-15 20:50:20 +0000

    Add find_preferred_language

--- callament/l10n.py
+++ callament/l10n.py
@@ -1,8 +1,12 @@
 import re
-from typing import List
+from typing import List, Optional
 
 
 Q_VALUE_RE = r"^(?:0(?:\.[0-9]{0,3})?|1(?:\.0{0,3})?)$"
+
+
+class LanguageNotAvailableException(Exception):
+    pass
 
 
 def parse_accept_language(spec: str) -> List[str]:
@@ -34,3 +38,54 @@ def parse_accept_language(spec: str) -> List[str]:
     pairs.sort(reverse=True, key=lambda pair: pair[1])
     # Cut off the q-value, the caller is probably not interested in it.
     return [pair[0] for pair in pairs]
+
+
+def find_preferred_language(
+    *,
+    prefs: List[str],
+    available: List[str],
+    fallback: Optional[str] = None,
+) -> str:
+    """From a list of available languages, select the one most preferred.
+
+    The preferences list should be ordered with the most preferred language
+    first, just like `parse_accept_language` returns it.
+
+    The matching is performed as described under "Basic Filtering" (section
+    3.3.1) in RFC 4647: Either the preference matches an available language
+    exactly, or the preference is a prefix of an available language (e.g.
+    `de-de` would match `de-DE-1996`). Matching is case insensitive.
+
+    If no preferred language is available, either return the fallback (if
+    specified), or raise a LanguageNotAvailableExecption. However, if there is
+    an asterisk `*` in the preferences, the first item in `available` will be
+    selected instead of the fallback or raising an exception.
+    """
+    if not len(available):
+        raise ValueError("there should be at least one available language")
+
+    for preference in prefs:
+        preference = preference.lower()
+
+        # Look for an exact match.
+        for av in available:
+            if preference == av.lower():
+                return av
+
+        # If there was no exact match, try a prefix match.
+        prefix = f"{preference}-"
+        for av in available:
+            if av.lower().startswith(prefix):
+                return av
+
+    # No available language matched. If the wildcard is accepted, use the first
+    # available language.
+    if "*" in prefs:
+        return available[0]
+
+    # If there is a fallback, return that. Else, raise an exception.
+    if fallback is not None:
+        return fallback
+
+    raise LanguageNotAvailableException(
+        "none of the preferred languages are available")
--- tests/test_l10n.py
+++ tests/test_l10n.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Literal, Optional, Union
 
 import pytest
 
@@ -16,3 +16,39 @@ import callament.l10n as l10n
 ])
 def test_parse_accept_language(header: str, expected: List[str]):
     assert l10n.parse_accept_language(header) == expected
+
+
+@pytest.mark.parametrize("prefs,available,fallback,expected", [
+    (["de-de", "en"], ["de-DE-1996", "en-US"], None, "de-DE-1996"),
+    (["de-de", "en-GB"], ["de-AT", "en-US"], "", ""),
+    (["de-de", "en-GB", "*"], ["de-AT", "en-US"], None, "de-AT"),
+    (["de-de", "en"], ["de-AT", "en-US"], "", "en-US"),
+    (["de-de", "en", "*"], ["de-AT", "en-US"], None, "en-US"),
+    (["de-de", "de", "en", "*"], ["de-AT", "de-DE", "en-US"], None, "de-DE"),
+    (["de-de", "tlh"], ["de-AT", "de-DE", "en-US"], None, "de-DE"),
+    (["de-de", "tlh"], ["de-AT", "en-US"], None, False),
+])
+def test_find_preferred_language(
+    prefs: List[str],
+    available: List[str],
+    fallback: Optional[str],
+    expected: Union[str, Literal[False]],
+):
+    if expected is False:  # parameter means "expect an exception"
+        with pytest.raises(l10n.LanguageNotAvailableException):
+            l10n.find_preferred_language(
+                prefs=prefs,
+                available=available,
+                fallback=fallback,
+            )
+    else:
+        assert l10n.find_preferred_language(
+            prefs=prefs,
+            available=available,
+            fallback=fallback,
+        ) == expected
+
+
+def test_find_preferred_with_no_available_languages():
+    with pytest.raises(ValueError):
+        l10n.find_preferred_language(prefs=["de-DE", "*"], available=[])
