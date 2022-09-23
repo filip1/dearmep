2657612a4cb28519f21380112ac0578c631b9226
Tim Weber <scy@scy.name>
2022-09-23 11:30:51 +0000

    Track l10n results in Prometheus

--- callament/api/v1.py
+++ callament/api/v1.py
@@ -1,8 +1,17 @@
 from os import environ
+
 from fastapi import APIRouter, Header, Request
+from prometheus_client import Counter
 
 from ..l10n import find_preferred_language, get_country, parse_accept_language
 from ..models import LanguageDetection, LocalizationResponse
+
+
+l10n_autodetect_counter = Counter(
+    "l10n_autodetect_total",
+    "Number of times language/country autodetection was performed, by results",
+    ["language", "country"],
+)
 
 
 router = APIRouter()
@@ -18,6 +27,11 @@ def localize(request: Request, accept_language: str = Header("")):
     geo_db = environ.get("GEO_DB", "")
 
     preferences = parse_accept_language(accept_language)
+    recommended_lang = find_preferred_language(
+                prefs=preferences,
+                available=available_languages,
+                fallback=available_languages[0],
+            )
 
     location = get_country(
         geo_db,
@@ -25,14 +39,15 @@ def localize(request: Request, accept_language: str = Header("")):
         request.client.host if request.client else "",
     )
 
+    # Track localization results in Prometheus.
+    l10n_autodetect_counter.labels(
+        recommended_lang, str(location.country)
+    ).inc()
+
     return LocalizationResponse(
         language=LanguageDetection(
             available=available_languages,
-            recommended=find_preferred_language(
-                prefs=preferences,
-                available=available_languages,
-                fallback=available_languages[0],
-            ),
+            recommended=recommended_lang,
             user_preferences=preferences,
         ),
         location=location,
