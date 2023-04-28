e1f20e4b767e3884501bd1316a04b7060428d341
Tobias Muehlberger <tobias@muehlberger.dev>
2023-04-28 21:58:50 +0300

    Make the location of static assets configurable + Added comments to describe config-options + Added better error-messages for required options

--- dearmep-client/src/app/common/services/base-url.service.ts
+++ dearmep-client/src/app/common/services/base-url.service.ts
@@ -2,12 +2,22 @@ import { Injectable } from '@angular/core';
 import { map, Observable, ReplaySubject } from 'rxjs';
 import { UrlUtil } from '../util/url.util';
 
+/**
+ * This service helps to convert urls that are supposed to be relative to the backend of this application
+ * into absolute urls. 
+ * Since the brownsers behavior is to treate all relative urls as if they were referencing to the embedding
+ * host-page it is necessary to convert relative urls to absolute using a configurable base-url. 
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class BaseUrlService {
   private readonly baseUrl$ = new ReplaySubject<string>()
+  private readonly assetsPath$ = new ReplaySubject<string>()
 
+  /**
+   * Sets the base-url that will be used as a base for converting relative urls
+   */
   public setBaseUrl(url: string) {
     if (!UrlUtil.isAbsolute(url)) {
       throw new Error("BaseUrl must be absolute")
@@ -18,7 +28,7 @@ export class BaseUrlService {
   public getBaseUrl$(): Observable<string> {
     return this.baseUrl$.asObservable()
   }
-
+  
   public toAbsoluteUrl$(relativeUrl: string): Observable<string> {
     return this.baseUrl$.pipe(
       map(baseUrl => UrlUtil.toAbsolute(relativeUrl, baseUrl))
