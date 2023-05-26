98d2b2a4fdb7bf379cc42bb76102eaa5a1c57160
Tim Weber <scy@scy.name>
2023-05-26 11:24:32 +0000

    Add EP Roll Call Vote reader back again

--- dearmep/cli/convert.py
+++ dearmep/cli/convert.py
@@ -5,7 +5,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from . import Context
 from ..convert import csv, json
-from ..progress import FlexiStrReader
+from ..convert.europarl import rollcallvote
+from ..progress import FlexiBytesReader, FlexiStrReader
 
 
 def run_csv2json(ctx: Context):
@@ -17,6 +18,18 @@ def run_csv2json(ctx: Context):
             ascii_only=not ctx.args.non_ascii,
         ):
             print(jsonstr)
+
+
+def rollcallvote_topics(ctx: Context):
+    with ctx.task_factory() as tf:
+        table = rollcallvote.list_topics(ctx.args.input, tf)
+    ctx.console.print(table.to_rich_table())
+
+
+def rollcallvote_votes(ctx: Context):
+    with ctx.task_factory() as tf:
+        table = rollcallvote.list_votes(ctx.args.input, tf, ctx.args.topic)
+    ctx.console.print(table.to_rich_table())
 
 
 def add_parser(subparsers: _SubParsersAction, help_if_no_subcommand, **kwargs):
@@ -48,5 +61,31 @@ def add_parser(subparsers: _SubParsersAction, help_if_no_subcommand, **kwargs):
         help="don't escape non-ASCII characters in the JSON output",
     )
     csv2json.set_defaults(func=run_csv2json, raw_stdout=True)
+
+    rcv = subsub.add_parser(
+        "europarl.rollcallvote",
+        help="European Parliament Roll Call Vote",
+    )
+    rcv_sub = rcv.add_subparsers(metavar="RCV_ACTION")
+
+    rcv_topics = rcv_sub.add_parser(
+        "topics",
+        help="list all voting topics in the input file",
+    )
+    FlexiBytesReader.add_as_argument(rcv_topics)
+    rcv_topics.set_defaults(func=rollcallvote_topics, raw_stdout=True)
+
+    rcv_votes = rcv_sub.add_parser(
+        "votes",
+        help="list all votes for/against a given topic",
+    )
+    rcv_votes.add_argument(
+        "topic",
+        help="ID of the topic to return the votes for",
+    )
+    FlexiBytesReader.add_as_argument(rcv_votes)
+    rcv_votes.set_defaults(func=rollcallvote_votes, raw_stdout=True)
+
+    help_if_no_subcommand(rcv)
 
     help_if_no_subcommand(parser)
---
+++ dearmep/convert/europarl/rollcallvote.py
@@ -0,0 +1,74 @@
+from xml.dom import pulldom
+
+from defusedxml.pulldom import parse as pulldom_parse  # type: ignore[import]
+
+from ...progress import BaseTaskFactory, FlexiBytesReader
+from ..tabular import Tabular
+from ..xml import get_text
+
+
+DESCRIPTION_TAG = "RollCallVote.Description.Text"
+RESULT_TAG = "RollCallVote.Result"
+GROUP_TAG = "Result.PoliticalGroup.List"
+MEMBER_TAG = "PoliticalGroup.Member.Name"
+
+RESULT_MAP = {
+    "Result.For": "+",
+    "Result.Against": "-",
+    "Result.Abstention": "0",
+}
+
+
+def list_topics(input: FlexiBytesReader, tf: BaseTaskFactory) -> Tabular:
+    table = Tabular("ID", "Description")
+    with tf.create_task("parsing XML") as task:
+        input.set_task(task)
+        with input as stream:
+            doc = pulldom_parse(stream)
+            for ev, node in doc:
+                if ev == pulldom.START_ELEMENT and node.tagName == RESULT_TAG:
+                    topic_id = node.getAttribute("Identifier")
+                elif ev == pulldom.START_ELEMENT \
+                        and node.tagName == DESCRIPTION_TAG:
+                    doc.expandNode(node)
+                    topic_text = get_text(node)
+                    table.append(topic_id, topic_text)
+    return table
+
+
+def list_votes(
+    input: FlexiBytesReader,
+    tf: BaseTaskFactory,
+    topic: str,
+) -> Tabular:
+    table = Tabular("Group", "MEPID", "PersID", "Name", "Vote")
+    with tf.create_task("parsing XML") as task:
+        input.set_task(task)
+        with input as stream:
+            doc = pulldom_parse(stream)
+            for ev, node in doc:
+                if ev != pulldom.START_ELEMENT or node.tagName != RESULT_TAG:
+                    continue
+                if node.getAttribute("Identifier") != topic:
+                    continue
+                doc.expandNode(node)
+                for vote_child in node.childNodes:
+                    if vote_child.tagName not in RESULT_MAP:
+                        continue
+                    vote = RESULT_MAP[vote_child.tagName]
+                    for group_child in vote_child.childNodes:
+                        if group_child.tagName != GROUP_TAG:
+                            continue
+                        group = group_child.getAttribute("Identifier")
+                        for person_child in group_child.childNodes:
+                            if person_child.tagName != MEMBER_TAG:
+                                continue
+                            table.append(
+                                group,
+                                person_child.getAttribute("MepId"),
+                                person_child.getAttribute("PersId"),
+                                get_text(person_child),
+                                vote,
+                            )
+                return table
+            raise KeyError(f"topic {topic} not found")
