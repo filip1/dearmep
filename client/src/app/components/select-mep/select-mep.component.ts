10d7762eb14edabddbd536bbda94bc3bf992752b
Tobias Muehlberger <tobias@muehlberger.dev>
2023-07-21 10:47:09 +0200

    Lint fix

--- client/src/app/components/select-mep/select-mep.component.ts
+++ client/src/app/components/select-mep/select-mep.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { combineLatest, filter, map, Observable, startWith, tap } from 'rxjs';
+import { combineLatest, filter, map, Observable, startWith } from 'rxjs';
 import { DestinationRead, DestinationSearchResult } from 'src/app/api/models';
 import { CallingStep } from 'src/app/model/calling-step.enum';
 import { CallingStateManagerService } from 'src/app/services/calling/calling-state-manager.service';
