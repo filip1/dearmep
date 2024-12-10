8e23250d54f38d83de10c3d7aaf05e4a3dbb647c
Tim Weber <scy@scy.name>
2024-12-10 23:20:46 +0000

    Fix SIM212 (twisted `if`)
    Yeah, should've done that in 19b223c0b59507e06d0827c9d678a74bb0cb4446
    already. 🤦
    
    Signed-off-by: Tim Weber <scy@scy.name>

--- server/dearmep/config.py
+++ server/dearmep/config.py
@@ -483,7 +483,7 @@ class Settings(BaseSettings):
     def empty_string_is_none(
         cls, v: Optional[DirectoryPath]
     ) -> Optional[DirectoryPath]:
-        return None if not v else v
+        return v or None
 
 
 def is_config_missing(e: ValidationError) -> bool:
