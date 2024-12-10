1133d5f70f8b2598f63c02d59225f7ea3b13c527
Tim Weber <scy@scy.name>
2024-12-10 22:34:34 +0000

    Ignore S408 (`minidom` import) for now
    This is a security warning, but I'm only importing types that don't have
    a defusedxml alternative anyway. Furthermore, there should be no
    warnings in a `TYPE_CHECKING` block. I've opened
    <https://github.com/astral-sh/ruff/issues/14901> for this.
    
    Signed-off-by: Tim Weber <scy@scy.name>

--- server/dearmep/convert/xml.py
+++ server/dearmep/convert/xml.py
@@ -2,11 +2,13 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from typing import Union, cast
-from xml.dom.minidom import Element, Text
+from typing import TYPE_CHECKING, Union, cast
+
+if TYPE_CHECKING:
+    from xml.dom.minidom import Element, Text  # noqa: S408
 
 
-def get_text(node: Union[Element, Text]) -> str:
+def get_text(node: Union["Element", "Text"]) -> str:
     """Recursively concatenate text nodes in the `node`."""
     if node.nodeType == node.TEXT_NODE:
         # This casting is plain ugly, but otherwise mypy doesn't know it's str.
