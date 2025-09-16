filip1/dearmep

commit 28d816bcb383987db9776e562162f274c2f91ed1

Support reading zstd Parltrack MEP dumps
These have been the default since 2024-11-11.

The options `--lz` and `--no-lz` have been deprecated;
`--compression=zstd` is the new default. However, `--compression=lz` and
`--compression=none` are supported, too, and the deprecated options map
to them.

Fixes #305.

Signed-off-by: Tim Weber <scy@scy.name>

server/dearmep/cli/convert.py
@@ -61,13 +61,16 @@ def convert_audio(ctx: Context) -> None:
 
 
 def parltrack_meps(ctx: Context) -> None:
+    compression_str = ctx.args.compression
+    if ctx.args.lz is not None:
+        compression_str = "lz" if ctx.args.lz else "none"
     with ctx.task_factory() as tf:
         for output in dump.dump_iter_json(
             mep.convert_meps(
                 ctx.args.input,
                 tf,
                 include_inactive=ctx.args.include_inactive,
-                lz_compressed=ctx.args.lz,
+                compression=mep.Compression[compression_str.upper()],
             )
         ):
             print(output)  # noqa: T201
@@ -285,25 +288,40 @@ def add_parser(
         "destinations`) as the list of Destinations to contact.",
     )
     FlexiBytesReader.add_as_argument(meps)
-    meps_lz = meps.add_mutually_exclusive_group()
-    meps_lz.add_argument(
+    meps_compression = meps.add_mutually_exclusive_group()
+    meps_compression.add_argument(
+        "--compression",
+        metavar="METHOD",
+        choices=[method.name.lower() for method in mep.Compression],
+        help="assume the input to be compressed using METHOD; choices are "
+        "`none` (uncompressed JSON), `lz` (used by Parltrack until 2024), or "
+        "`zstd` (used by Parltrack since 2024, the default)",
+        default="zstd",
+    )
+    meps_compression.add_argument(
         "--lz",
         action="store_true",
-        help="assume the input to be lz compressed, just as you would "
-        "download it from the Parltrack website (default)",
+        deprecated=True,
+        help="deprecated alias for --compression=lz",
     )
-    meps_lz.add_argument(
+    meps_compression.add_argument(
         "--no-lz",
         dest="lz",
         action="store_false",
-        help="assume the input to be uncompressed JSON",
+        deprecated=True,
+        help="deprecated alias for --compression=none",
     )
     meps.add_argument(
         "--include-inactive",
         action="store_true",
         help='include MEPs that are marked in the input as being "inactive"',
     )
-    meps.set_defaults(func=parltrack_meps, raw_stdout=True, lz=True)
+    meps.set_defaults(
+        func=parltrack_meps,
+        raw_stdout=True,
+        compression="zstd",
+        lz=None,
+    )
 
     audio = subsub.add_parser(
         "audio",

server/dearmep/convert/parltrack/mep.py
@@ -6,9 +6,11 @@ import json
 import re
 from collections.abc import Generator, Iterable
 from datetime import date, datetime, timezone
+from enum import Enum
 from typing import (
     TYPE_CHECKING,
     Any,
+    Optional,
     Union,
     cast,
 )
@@ -47,6 +49,12 @@ LOCATION_MAP = {
 }
 
 NON_FILENAME_SAFE = re.compile(r"[^\w\d _-]")
+
+
+class Compression(Enum):
+    NONE = 0
+    LZ = 1
+    ZSTD = 2
 
 
 def is_current(item: dict) -> bool:
@@ -182,18 +190,24 @@ def convert_meps(
     tf: BaseTaskFactory,
     *,
     include_inactive: bool = False,
-    lz_compressed: bool = True,
+    compression: Optional[Compression] = None,
 ) -> Iterable[DumpableModels]:
-    dc_str = " and decompressing" if lz_compressed else ""
+    if compression is None:
+        compression = Compression.NONE
+    dc_str = "" if compression == Compression.NONE else " and decompressing"
     with tf.create_task(f"reading{dc_str} input") as task:
         input.set_task(task)
         with input as input_stream:
-            if lz_compressed:
+            if compression == Compression.LZ:
                 from ..lz import lz_decompressor
 
                 src: Union[BufferedReader, Iterable[bytes]] = lz_decompressor(
                     input_stream
                 )
+            elif compression == Compression.ZSTD:
+                from ..zstd import zstd_decompressor
+
+                src = zstd_decompressor(input_stream)
             else:
                 src = input_stream
             # TODO: If src was a file-like, not a generator, we could skip this

server/dearmep/convert/zstd.py
@@ -1,0 +1,13 @@
+# SPDX-FileCopyrightText: © 2025 Tim Weber
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from collections.abc import Iterable
+from io import BufferedReader
+
+import zstandard
+
+
+def zstd_decompressor(input: BufferedReader) -> Iterable[bytes]:
+    dctx = zstandard.ZstdDecompressor()
+    return dctx.read_to_iter(input)

server/pyproject.toml
@@ -96,6 +96,7 @@ convert = [
 	"csvkit>=2",
 	"visidata>=3",
 	"xlsxwriter~=3.1",
+	"zstandard~=0.24",
 ]
 specs = [
 	"eralchemy2~=1.4",

commit b6ac8152f72b88e191be3b4b3df4e7c7e94f47a8

Fix argparse usage for Python < 3.13
`deprecated` is new in 3.13 and will cause an error on previous Python
versions.

Bug introduced by 28d816bcb383987db9776e562162f274c2f91ed1 (#311, #305).

Signed-off-by: Tim Weber <scy@scy.name>

server/dearmep/cli/convert.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import logging
+import sys
 from pathlib import Path
 from typing import TYPE_CHECKING, Callable
 
@@ -25,6 +26,9 @@ from ..progress import FlexiBytesReader
 
 MEP_PORTRAIT_FILE_PATTERN = "{id}.jpg"
 MEP_NAME_AUDIO_FILE_PATTERN = "{id}.mp3"
+
+# The "deprecated" argument exists in argparse since Python 3.13.
+DEPRECATED_ARG = {"deprecated": True} if sys.version_info >= (3, 13) else {}
 
 
 _logger = logging.getLogger(__name__)
@@ -301,15 +305,15 @@ def add_parser(
     meps_compression.add_argument(
         "--lz",
         action="store_true",
-        deprecated=True,
         help="deprecated alias for --compression=lz",
+        **DEPRECATED_ARG,  # type: ignore[arg-type]
     )
     meps_compression.add_argument(
         "--no-lz",
         dest="lz",
         action="store_false",
-        deprecated=True,
         help="deprecated alias for --compression=none",
+        **DEPRECATED_ARG,  # type: ignore[arg-type]
     )
     meps.add_argument(
         "--include-inactive",